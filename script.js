39566eea1a40337afea3908f04c3afeb095c90f8
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2024-01-03 11:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function activateSearch() {
+  const searchDiv = document.getElementById('searchDiv');
+  const searchInput = document.getElementById('searchInput');
+
+  searchDiv.classList.add('active');
+  searchInput.focus();
+}
